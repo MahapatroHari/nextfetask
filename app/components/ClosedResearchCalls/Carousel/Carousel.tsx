8d7d77905caebe00de7771feea2beb310f2978db
harishankar-KP <hari.mahapatro@kraftpixel.com>
2025-01-13 11:53:48 +0530

    closed research calls with glider

--- app/components/ClosedResearchCalls/Carousel/Carousel.tsx
+++ app/components/ClosedResearchCalls/Carousel/Carousel.tsx
@@ -9,11 +9,34 @@ import "glider-js/glider.min.css";
 
 export default function Carousel() {
   return (
-    <div className={`container ${styles.pageContainer}`}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className={`container ${styles.carouselContainer}`}>
+      <div className={`${styles.cardContainerResearch} researchSlider`}>
+        <Glider
+          draggable
+          hasDots
+          slidesToShow={3}
+          slidesToScroll={1}
+          skipTrack={true}
+          itemWidth={334}
+          exactWidth
+        >
+          <div className={styles.sliderContainer}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </Glider>
+      </div>
+
+      <div className={styles.mobileCardContainer}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
+
+//Here
